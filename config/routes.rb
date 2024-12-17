6741c7fc90eb63f226f92d12340ce711bacd5ff9
yyyrk <andrei.iurik@gmail.com>
2024-12-17 07:04:11 +0300

    chore: отдал статичные страницы  в обход рельсового приложения для скорости

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  get '/about', to: 'static_pages#about'
-
-  get '/about/author', to: 'static_pages#author'
+  # get '/about', to: 'static_pages#about'
+  #
+  # get '/about/author', to: 'static_pages#author'
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
