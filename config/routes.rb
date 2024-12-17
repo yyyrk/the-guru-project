6bf25b1efaa89b7a7755fa7eddf8ec310f0d25dc
yyyrk <andrei.iurik@gmail.com>
2024-12-17 12:22:30 +0300

    chore: удалил лишние комментарии и файлы

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-
-  # get '/about', to: 'static_pages#about'
-  #
-  # get '/about/author', to: 'static_pages#author'
-
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
