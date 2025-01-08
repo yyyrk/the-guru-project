6caf9eb59b7f95e6c16db1d92bc4fb21e2f88ede
yyyrk <andrei.iurik@gmail.com>
2025-01-08 08:06:23 +0300

    fix: remove associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,2 @@
 class Category < ApplicationRecord
-  has_many :tests, dependent: :destroy
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,2 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  has_many :questions, dependent: :destroy
 end
