06c4375db90b401febd5270d9eb16271c3408b9b
yyyrk <andrei.iurik@gmail.com>
2025-01-08 08:42:01 +0300

    chore: update security and create migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_24_115658) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_08_052529) do
   create_table "answers", force: :cascade do |t|
     t.string "content", null: false
     t.boolean "correct", default: false, null: false
@@ -36,10 +36,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_24_115658) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 0, null: false
+    t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id", default: 0, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
