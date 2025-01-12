0071ee050720cc960878654f1b6980577fa10391
yyyrk <andrei.iurik@gmail.com>
2025-01-12 17:24:56 +0300

    fix: def tests_by_level(level)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Выдает тесты, принимая level в качествен аргумента
   def tests_by_level(level)
-    tests.where(level: level)
+    Test.joins("INNER JOIN test_results ON test_results.test_id = tests.id")
+        .where("test_results.user_id = ? AND tests.level = ?", id, level)
   end
 end
