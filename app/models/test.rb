908954edd40a1784210f1cd6db1ca65f795b8317
yyyrk <andrei.iurik@gmail.com>
2025-01-12 08:13:06 +0300

    chore: rubocop -a

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.tests_by_category_name(category_name)
-    joins('INNER JOIN categories ON categories.id = tests.category_id')
-      .where('categories.name = ?', category_name)
+    joins("INNER JOIN categories ON categories.id = tests.category_id")
+      .where("categories.name = ?", category_name)
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   # Выдает тесты, принимая level в качествен аргумента
   def tests_by_level(level)
     tests.where(level: level)
