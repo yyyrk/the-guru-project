1ff5eb6a8253484ad1c4cf0d64d5a4220bec3724
yyyrk <andrei.iurik@gmail.com>
2025-01-16 19:09:39 +0300

    fix: models #2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,18 +1,23 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  belongs_to :author, class_name: "User", optional: true
+  # В комментариях постарался объяснить логику и вроде она соответствует ER. После проверки комментарии удалю.
 
-  has_many :test_results, dependent: :destroy
-  has_many :users, through: :test_results
+  # 1. Связь многие ко многим с Users через TestResult
+  has_many :test_results, dependent: :destroy # Показал здесь связь с промежуточной таблицей
+  has_many :users, through: :test_results     # Показал связь "многие ко многим" с Users через TestResult
+
+  # 2. Связь "многие к одному" с Users с колонкой author_id
+  belongs_to :author, class_name: "User", foreign_key: :author_id, optional: true
+
+  # 3.Связь "многие к одному" с Category с колонкой category_id
+  belongs_to :category
+
+  # 4. Связь "один ко многим" с Questions
   has_many :questions, dependent: :destroy
 
-  def self.by_category_name(category_name)
+  def self.titles_by_category_name(category_name)
     joins(:category)
       .where(categories: { name: category_name })
       .order(title: :desc)
-  end
-
-  def self.titles_by_category_name(category_name)
-    by_category_name(category_name).pluck(:title)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :test_results, dependent: :destroy
+  has_many :test_results
   has_many :tests, through: :test_results
 
   # Тесты, которые создал пользователь
   has_many :authored_tests, class_name: "Test", foreign_key: :author_id
+
   def tests_by_level(level)
     tests.where(level: level)
   end
