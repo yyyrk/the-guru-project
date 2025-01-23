31ff34c3cf6cb715f600ce1da929ec9fdd6830b1
yyyrk <andrei.iurik@gmail.com>
2025-01-23 19:11:29 +0300

    fix: (error) bin/rails db:test:prepare test test:system

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :author,
              class_name: "User",
-             foreign_key: :author_id,
-             optional: true
+             foreign_key: :author_id
   belongs_to :category
 
   has_many :test_results, dependent: :destroy
