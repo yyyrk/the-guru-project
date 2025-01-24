8e044d305b3ebe108c5c2a796a960b47e47a8e4b
yyyrk <andrei.iurik@gmail.com>
2025-01-24 18:34:17 +0300

    fix: rollback migration

--- db/migrate/20250124115357_add_indexes_and_foreign_keys_to_test_results.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexesAndForeignKeysToTestResults < ActiveRecord::Migration[8.0]
-  def change
-    add_index :test_results, :user_id
-    add_index :test_results, :test_id
-
-    add_foreign_key :test_results, :users
-    add_foreign_key :test_results, :tests
-  end
-end
