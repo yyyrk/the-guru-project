0f944a5294945dc4aeb5b25790dac1255405af10
yyyrk <andrei.iurik@gmail.com>
2025-02-01 09:46:33 +0300

    chore: rubocop -a

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,7 +9,7 @@ class Test < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  scope :easy, -> { where(level: [0, 1]) }
+  scope :easy, -> { where(level: [ 0, 1 ]) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where("level >= ?", 5) }
   scope :by_level, ->(level) { where(level: level) }
