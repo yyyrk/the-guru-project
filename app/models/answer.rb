b34a04db85919cab5536b8930264f54c90bc012c
yyyrk <andrei.iurik@gmail.com>
2025-02-01 09:31:41 +0300

    feat: add validations and scope

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+
+  validate :validate_answers_count, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  def validate_answers_count
+    return if question.answers.count < 4
+
+    errors.add(:question, "Too many answers!!! (must be less then 5!!!)")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  has_many :test_results, dependent: :destroy # чтобы при удалении юзера удалялись и результаты
-  has_many :tests, through: :test_results, dependent: :destroy # соответв. будут удаляться и тесты при удалении юзера
+  has_many :test_results, dependent: :destroy
+  has_many :tests, through: :test_results, dependent: :destroy
   has_many :author_tests, class_name: "Test", foreign_key: "author_id"
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false } # чтобы игнорировать регистр
 
   def tests_by_level(level)
     tests.where(level: level)
