fd154ac5940a0f4618fdb00dcc59f2111fed2a6d
yyyrk <andrei.iurik@gmail.com>
2025-02-06 11:45:33 +0300

    feat: add controllers (tests, questions, answers) and fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get "static_pages/about"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tests do
+    resources :questions, only: [:index, :show, :create, :destroy]
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :tests, except: [:index, :show] # Уже объявил выше как родительский ресурс
+
+  get "about", to: "static_pages#about"
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
