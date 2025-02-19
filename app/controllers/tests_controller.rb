825c3cf634abfd9d5acf3579e2166d0df09d01f9
yyyrk <andrei.iurik@gmail.com>
2025-02-19 13:08:20 +0300

    feat: add question deletion and fix form 🚀

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,11 @@
 class TestsController < ApplicationController
+  # skip_before_action :find_test
+  before_action :find_test, only: %i[show]
+  after_action :send_log_message
+  around_action :log_execute_time
+
+rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
   def index
     result = [ "Class: #{params.class}", "Parameters: #{params.inspect}" ]
 
@@ -6,7 +13,9 @@ class TestsController < ApplicationController
   end
 
   def show
-    redirect_to root_path
+    title = Test.first.title
+
+    render inline: '<%= @test.title %>'
   end
 
   def new
@@ -28,5 +37,25 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+
+    def send_log_message
+      logger.info("Action [#{action_name}] was finished")
+    end
+  end
+
+  def log_execute_time
+    start = Time.now
+    yield
+    finish = Time.now - start
+
+    logger.info("Execution time: #{finish * 1000}ms")
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
   end
 end
